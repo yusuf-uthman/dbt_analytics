21daa4209b27bd01217f43d0306e851a53453f74
Uthman Yusuf <yusufuthman57+class01@gmail.com>
2025-07-16 23:23:55 +0000

    fix issues with sample project

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -14,11 +14,11 @@ with source_data as (
     select 1 as id
     union all
     select null as id
-
 )
 
 select *
 from source_data
+
 
 /*
     Uncomment the line below to remove records with null `id` values
