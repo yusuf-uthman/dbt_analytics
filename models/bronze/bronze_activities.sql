1b7584865b3b7c972c84bc6c1b06b02202b74168
Oluwadara Fabunmi <darajohn.fo@gmail.com>
2025-08-23 22:11:25 +0000

    activities created

---
+++ models/bronze/bronze_activities.sql
@@ -0,0 +1,12 @@
+{{config(materialized = 'table')}}
+
+with 
+
+source as (
+
+    select activity_id, opportunity_id, rep_id, activity_type, duration_minutes, outcome, 'group_3_sales' as business 
+    from {{ source('RAW_GROUP_3_DBT_CLASS', 'ACTIVITIES') }}
+
+)  
+select distinct(activity_id) from source
+ where opportunity_id is null
