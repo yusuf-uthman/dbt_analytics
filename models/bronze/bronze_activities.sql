ab64d120344c6663d69581ffc8d535833ba3a5b0
Oluwadara Fabunmi <darajohn.fo@gmail.com>
2025-08-25 05:53:59 +0000

    Changes made

--- models/bronze/bronze_activities.sql
+++ models/bronze/bronze_activities.sql
@@ -4,12 +4,27 @@ with
 
 source as (
 
-    select activity_id, opportunity_id, rep_id, activity_type, duration_minutes, outcome, 'group_3_sales' as business 
+    select 
+        activity_id, 
+        opportunity_id, 
+        rep_id, 
+        activity_type, 
+        duration_minutes, 
+        outcome, 
+        'group_3_sales' as business 
     from {{ source('RAW_GROUP_3_DBT_CLASS', 'ACTIVITIES') }}
 
 )  
 
-select activity_id, opportunity_id, rep_id, activity_type, duration_minutes, outcome, business from source
+select 
+    activity_id, 
+    opportunity_id, 
+    rep_id, 
+    activity_type, 
+    duration_minutes, 
+    outcome, 
+    business 
+from source
  
 
  
--- tests/bronze_activities_test.sql
+++
@@ -1,2 +0,0 @@
-select distinct(activity_id) from {{ source('RAW_GROUP_3_DBT_CLASS', 'ACTIVITIES') }}
- where opportunity_id is null
