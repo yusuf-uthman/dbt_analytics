e986d8bbf10d1772e7b2233cf9e8571596e9ef13
Chidinma Okeh <chidinmaokeh75@gmail.com>
2025-08-26 16:24:46 +0000

    Using CTEs to write the SQL statement

--- models/bronze/bronze_sales_reps.sql
+++ models/bronze/bronze_sales_reps.sql
@@ -1,8 +1,32 @@
 
-Select 
-        REP_ID, LAST_NAME, FIRST_NAME,  REGION, QUOTA, EMAIL, HIRE_DATE,
-            'group_3_sales' As business
-                from {{ source('RAW_GROUP_3_DBT_CLASS', 'SALES_REPS') }}
-                        {{ config(
-                                     materialized='table'
-                            ) }}
+
+{{ config(
+    materialized='table'
+) }}
+
+WITH source AS (
+    SELECT
+        REP_ID AS rep_id,
+        FIRST_NAME AS first_name,
+        LAST_NAME AS last_name,
+        REGION,
+        QUOTA,
+        EMAIL,
+        HIRE_DATE,
+        'group_3_sales' AS business
+    FROM {{ source('RAW_GROUP_3_DBT_CLASS', 'SALES_REPS') }}
+)
+
+
+SELECT 
+REP_ID AS rep_id,
+
+        REGION,
+        first_name,
+        last_name,
+        QUOTA,
+        EMAIL,
+        HIRE_DATE,
+        business
+
+        FROM source
