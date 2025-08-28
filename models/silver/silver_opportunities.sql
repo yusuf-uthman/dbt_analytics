af4a9f51317a648469adb5b186d7ac975fcb1d09
Oluwadara Fabunmi <darajohn.fo@gmail.com>
2025-08-28 23:20:52 +0000

    Cleaning done on silver opportunities layer to meet criteria

--- models/silver/silver_opportunities.sql
+++ models/silver/silver_opportunities.sql
@@ -1,19 +1,29 @@
 with silver_opportunities as(
     select
-        opportunity_id,
-        rep_id,
-        account_name,
-        opportunity_value,
-        stage,
-        close_date,
-        product_type,
-        business
+        cast(opportunity_id as int) as cleaned_opportunity_id,
+        cast(rep_id as int) as cleaned_rep_id,
+        trim(upper(account_name)) as cleaned_account_name,
+        cast(opportunity_value as dec) as cleaned_opportunity_value,
+        trim(upper(stage)) as cleaned_stage,
+        cast(close_date as date) as cleaned_close_date,
+        year(close_date) as op_close_year,
+        month(close_date) as op_close_month,
+        day(close_date) as op_close_day,
+        trim(upper(product_type)) as cleaned_product_type,
+        trim(upper(business)) as cleaned_business,
     from bronze_opportunities
 )
 
-alter table silver_opportunities
-alter column rep_id int,
-alter column opportunity_id int;
-
-select *
+select 
+    cleaned_opportunity_id,
+    cleaned_rep_id,
+    cleaned_account_name,
+    cleaned_opportunity_value,
+    cleaned_stage,
+    cleaned_close_date,
+    op_close_year,
+    op_close_month,
+    op_close_day,
+    cleaned_product_type,
+    cleaned_business
 from silver_opportunities
